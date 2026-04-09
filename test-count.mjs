b8f14765f1811be675dbc6032d9f0034900eebb4
iktiar ramadani <ihtiartiar081@gmail.com>
2026-04-09 10:32:42 +0700

    pembersihan file tidak penting

--- test-count.mjs
+++
@@ -1,7 +0,0 @@
-import countapi from 'countapi-js';
-
-countapi.visits().then((result) => {
-    console.log("CountAPI visits:", result.value);
-}).catch((err) => {
-    console.error("CountAPI Error:", err);
-});
